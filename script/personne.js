b559d7115e9e46611d66f24b84b099f689bbf23e
KillianOBI1 <killian.monot@orange.fr>
2018-11-08 16:50:42 +0100

    add js and memo

---
+++ script/personne.js
@@ -0,0 +1,14 @@
+var Person = /** @class */ (function () {
+    function Person(firstName, lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    Object.defineProperty(Person.prototype, "fullName", {
+        get: function () {
+            return this.firstName + " " + this.lastName;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    return Person;
+}());
